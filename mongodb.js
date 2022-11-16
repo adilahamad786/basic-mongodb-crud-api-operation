65d48529cfd17d21c692ae5308e243a4c1de5de7
adilahamad786 <adilahamad.personally@gmail.com>
2022-11-16 21:16:17 +0530

    Somthing related to GUID.

--- mongodb.js
+++ mongodb.js
@@ -14,20 +14,20 @@ MongoClient.connect(connectionURL, { useNewUrlParser : true }, (error, client) =
 
     // Basic of GUID -> Global Unique Id
     const id = new ObjectId();
-    console.log(id);
-    console.log(id.id)
-    console.log(id.getTimestamp());
+    console.log(id); // id
+    console.log(id.id) // id buffer info
+    console.log(id.getTimestamp()); // get exact id generate timing
+    console.log(id.toHexString()); // convert object into string
 
-    // Insert one collection inside the "demo" Database and "users" Collection
-    db.collection('users').insertOne({
-        _id : id,
-        name : "Adil Ahamad",
-        age : 21
-    }, (error, result) => {
-        if (error)
-            return console.log("Unable to insert data!");
-        console.log(result);
-    });
+    // // Insert one collection inside the "demo" Database and "users" Collection
+    // db.collection('users').insertOne({
+    //     name : "Adil Ahamad",
+    //     age : 21
+    // }, (error, result) => {
+    //     if (error)
+    //         return console.log("Unable to insert data!");
+    //     console.log(result);
+    // });
 
     // // Insert one collection inside the "demo" Database and "users" Collection
     // db.collection('users').insertMany([
