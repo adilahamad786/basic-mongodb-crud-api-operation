488775335f267d38163d47f060231cdcc281e897
adilahamad786 <adilahamad.personally@gmail.com>
2022-11-16 22:57:29 +0530

    Delete a Documents from Collection which are store in Mongodb Database.

--- mongodb.js
+++ mongodb.js
@@ -82,27 +82,36 @@ MongoClient.connect(connectionURL, { useNewUrlParser : true }, (error, client) =
     //     console.log(result);
     // });
 
-    // Update a Document from the "users" Collection Which are present in "demo" Database.
-    db.collection('users').updateOne({
-        _id : new ObjectId('637504e2efa1658329047c30')
-    }, {
-        $set : {
-            name : "Aman",
-            age : 23
-        }
-    }).then(result => {
-        console.log(result);
-    }).catch(error => {
-        console.log(error);
-    });
+    // // Update a Document from the "users" Collection Which are present in "demo" Database.
+    // db.collection('users').updateOne({
+    //     _id : new ObjectId('637504e2efa1658329047c30')
+    // }, {
+    //     $set : {
+    //         name : "Aman",
+    //         age : 23
+    //     }
+    // }).then(result => {
+    //     console.log(result);
+    // }).catch(error => {
+    //     console.log(error);
+    // });
 
-    // Update Multiple Document (Increase age by 1) from the "users" Collection Which are present in "demo" Database.
-    db.collection('users').updateMany({
-        age : 21
-    },{
-        $inc : {
-            age : 1
-        }
+    // // Update Multiple Document (Increase age by 1) from the "users" Collection Which are present in "demo" Database.
+    // db.collection('users').updateMany({
+    //     age : 21
+    // },{
+    //     $inc : {
+    //         age : 1
+    //     }
+    // }).then(result => {
+    //     console.log(result);
+    // }).catch(error => {
+    //     console.log(error);
+    // });
+
+    // Delete a Document from the "users" Collection Which are present in "demo" Database.
+    db.collection('users').deleteOne({
+        name : "Babu"
     }).then(result => {
         console.log(result);
     }).catch(error => {
